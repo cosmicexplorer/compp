d3be9d4d4f99545802b9e28502a970d3afdafc6e
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-04-06 01:59:19 -0500

    improvement to test c file

--- test1.c
+++ test1.c
@@ -10,11 +10,13 @@
           */ (4)
 //#undef E
 
+#define A (2 + 3
+
 #define x (4 + y)
 #define y (2 * x)
 
 int main() {
-  int a = x + y + E;
+  int a = x + y + E + A);
   int b = y + x;
   printf("a: %d\n", a);
 }
